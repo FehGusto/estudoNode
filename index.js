936e9174f7a38038cc5f41fd4717d3f24383fdf1
FehGusto <felipe.santos.ofic@gmail.com>
2022-02-19 16:05:25 -0300

    Primeira Blibiotec - Aula-02

--- index.js
+++ index.js
@@ -5,13 +5,32 @@ function tratErro(erro) {
   throw new Error(chalk.red(erro.code, 'Não existe'));
 }
 
-function cacthArquiv(caminhArquivo) {
+async function cacthArquiv(caminhArquivo) {
   const enconding = 'utf-8'
-  fs.readFile(caminhArquivo, enconding, (erro, texto) => {
-    if(erro) {
-      tratErro(erro)
-    }
-    console.log(chalk.green(texto));
-  })
+  try {
+    const texto = await fs.promises.readFile(caminhArquivo, enconding)
+    console.log(chalk.green(texto))  
+  } catch(erro) {
+    tratErro(erro);
+  }
 }
+
+
+//function cacthArquiv(caminhArquivo) {
+//  const enconding = 'utf-8'
+//  fs.promises.readFile(caminhArquivo, enconding)
+//  .then((texto) => chalk.green(console.log(texto)))
+//  .catch((erro) => chalk.red(tratErro(erro)))
+//}
+
+//function cacthArquiv(caminhArquivo) {
+//  const enconding = 'utf-8'
+//  fs.readFile(caminhArquivo, enconding, (erro, texto) => {
+//    if(erro) {
+//      tratErro(erro)
+//    }
+//    console.log(chalk.green(texto));
+//  })
+//}
+
 cacthArquiv('./arquivos/texto1.md')
